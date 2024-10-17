ed6655c2586f02ff81f74775cc3dad2c6a149363
BigZetena <V.O.Sharipov@gmail.com>
2024-10-17 16:40:44 +0300

    add ainimate policy

--- react/units/footer/ui/footer.component.tsx
+++ react/units/footer/ui/footer.component.tsx
@@ -4,20 +4,20 @@ import React from 'react';
 export const Footer = () => {
   return (
     <div className="container max-w-container py-10 mx-auto font-raleway flex justify-between h-[170px]">
-      <div className="flex flex-col justify-between">
+      <div className="flex flex-col justify-between ">
         <div>
           <img src="/images/footer/holder.png" alt="holder" />
         </div>
         <p className="text-2xl">© БИТКЭМП, 2024</p>
       </div>
-      <div className="text-right flex flex-col justify-between">
+      <div className="text-right flex flex-col justify-between items-end">
         <p>
           196084, г. Санкт-Петербург, ул. Парфеновская д.12, БЦ “АКВИЛОН LINKS”
         </p>
         <a href="" className="">
           info@bit-camp.ru
         </a>
-        <div className="flex justify-between font-manrope">
+        <div className="flex justify-between font-manrope w-full">
           {' '}
           <a href="tel:+79811550991" className="">
             <strong>Телефон:</strong> +7 981 155 09 91
@@ -30,7 +30,14 @@ export const Footer = () => {
           </p>
         </div>
 
-        <Link href={'/policy'}>Политика конфиденциальности</Link>
+        <Link
+          className={
+            "w-max relative text-primary font-raleway font-bold transition-colors duration-300 after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-[2px] after:bg-primary after:transition-all after:duration-300 hover:after:w-full"
+          }
+          href={'/policy'}
+        >
+          Политика конфиденциальности
+        </Link>
       </div>
     </div>
   );
